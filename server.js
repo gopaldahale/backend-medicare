531ba9909154ed6d8d71254913755dfab9a1cc05
gopaldahale <gopaldahale00@gmail.com>
2026-06-06 23:56:36 +0530

    feat: add logging middleware for request origin and method

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log("Origin:", req.headers.origin);
+  console.log("Method:", req.method);
+  next();
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use('/api/users', userRoutes)
